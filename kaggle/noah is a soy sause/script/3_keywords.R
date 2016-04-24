305b9ab4dce3001dee0448119f14b43327507f9d
noahhhhhh <noah.xm.shaw@gmail.com>
2016-04-24 23:26:26 +1000

    dtm

--- kaggle/noah is a soy sause/script/3_keywords.R
+++ kaggle/noah is a soy sause/script/3_keywords.R
@@ -5,61 +5,192 @@ require(tm)
 require(SnowballC)
 require(stringr)
 require(ggplot2)
+source("script/utility.R")
 load("../../../data/RData/dt_jobs.RData")
 #######################################################################################
 ## hat = 1 ############################################################################
 #######################################################################################
-dt.hat.1 <- dt.jobs[hat == 1]
-title.hat.1 <- tolower(dt.hat.1$title)
-abstract.hat.1 <- tolower(dt.hat.1$abstract)
-raw_job_type.hat.1 <- tolower(dt.hat.1$raw_job_type)
+# dt.hat.1 <- dt.jobs[hat == 1]
+title.hat.1 <- tolower(dt.jobs[hat == 1]$title)
+abstract.hat.1 <- tolower(dt.jobs[hat == 1]$abstract)
+raw_job_type.hat.1 <- tolower(dt.jobs[hat == 1]$raw_job_type)
 
-#######################################################################################
-## title ##############################################################################
-#######################################################################################
-## remove punctuations and digits
-title.hat.1 <- gsub("[[:punct:][:digit:]]", " ", title.hat.1)
-## remove more than 2 space
-title.hat.1 <- gsub("\\s+", " ", title.hat.1)
-## remove non alpha
-title.hat.1 <- gsub("[^a-zA-Z]", " ", title.hat.1)
-## remove more than 2 space and strim spaces
-title.hat.1 <- str_trim(gsub("\\s+", " ", title.hat.1))
-## stemming
-title.hat.1 <- wordStem(title.hat.1)
-## remove stop words
-corp.title.hat.1 <- tm_map(Corpus(VectorSource(title.hat.1)), removeWords, stopwords("english")) 
-
-## dtm
-dtm.title.hat.1 <- DocumentTermMatrix(corp.title.hat.1)
-
-## data table
-dt.title.hat.1 <- data.table(text = unlist(sapply(corp.title.hat.1, `[`, "content")))
-
-## freq
-freq.title.hat.1 <- colSums(as.matrix(dtm.title.hat.1)) 
-length(freq.title.hat.1); length(title.hat.1)
-# [1] 12235
-# [1] 122886
-# order by frequency
-ord.title.hat.1 <- order(-freq.title.hat.1)
-# explore
-freq.title.hat.1[head(ord.title.hat.1)]
-# chef    cook barista   staff kitchen    cafe 
-# 23695   10945    7960    7733    7577    7184 
-head(table(freq.title.hat.1), 200)
-# 1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20 
-# 4630 1688  900  623  461  321  278  233  206  164  145  131  108  102   90   74   91   65   60   46 
-dt.wf.title.hat.1 <- data.table(word = names(freq.title.hat.1), freq = freq.title.hat.1)
+###########
+## title ##
+###########
+list.title.hat.1 <- txtPreprocess(title.hat.1)
+dt.title.hat.1 <- list.title.hat.1[["dt"]]
+dtm.title.hat.1 <- list.title.hat.1["dtm"]
+dt.wf.title.hat.1 <- list.title.hat.1[["dt.wf"]]
 
 # plot
-p <- ggplot(subset(dt.wf.title.hat.1, freq > 50), aes(word, freq))
+p <- ggplot(subset(dt.wf.title.hat.1, freq > 2000), aes(word, freq))
 p <- p + geom_bar(stat = "identity")   
 p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
 p
 
-# assoc
-findAssocs(dtms, "contrast", corlimit = .9)
+##############
+## abstract ##
+##############
+list.abstract.hat.1 <- txtPreprocess(abstract.hat.1, rm_sparse = .99999)
+dt.abstract.hat.1 <- list.abstract.hat.1[["dt"]]
+dtm.abstract.hat.1 <- list.abstract.hat.1["dtm"]
+dt.wf.abstract.hat.1 <- list.abstract.hat.1[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.abstract.hat.1, freq > 2000), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+##################
+## raw_job_type ##
+##################
+list.raw_job_type.hat.1 <- txtPreprocess(raw_job_type.hat.1)
+dt.raw_job_type.hat.1 <- list.raw_job_type.hat.1[["dt"]]
+dtm.raw_job_type.hat.1 <- list.raw_job_type.hat.1["dtm"]
+dt.wf.raw_job_type.hat.1 <- list.raw_job_type.hat.1[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.raw_job_type.hat.1, freq > 100), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+## save
+save(list.title.hat.1, list.abstract.hat.1, list.raw_job_type.hat.1, file = "../../../data/RData/dt_keywords_hat_1.RData")
+
+#######################################################################################
+## hat != -1 ############################################################################
+#######################################################################################
+# dt.hat.all <- dt.jobs
+title.hat.all <- tolower(dt.jobs$title)
+abstract.hat.all <- tolower(dt.jobs$abstract)
+raw_job_type.hat.all <- tolower(dt.jobs$raw_job_type)
+
+###########
+## title ##
+###########
+list.title.hat.all <- txtPreprocess(title.hat.all, rm_sparse = .999)
+dt.title.hat.all <- list.title.hat.all[["dt"]]
+dtm.title.hat.all <- list.title.hat.all["dtm"]
+dt.wf.title.hat.all <- list.title.hat.all[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.title.hat.all, freq > 2000), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+##############
+## abstract ##
+##############
+list.abstract.hat.all <- txtPreprocess(abstract.hat.all, rm_sparse = .993)
+dt.abstract.hat.all <- list.abstract.hat.all[["dt"]]
+dtm.abstract.hat.all <- list.abstract.hat.all["dtm"]
+dt.wf.abstract.hat.all <- list.abstract.hat.all[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.abstract.hat.all, freq > 2000), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+##################
+## raw_job_type ##
+##################
+list.raw_job_type.hat.all <- txtPreprocess(raw_job_type.hat.all, rm_sparse = .999)
+dt.raw_job_type.hat.all <- list.raw_job_type.hat.all[["dt"]]
+dtm.raw_job_type.hat.all <- list.raw_job_type.hat.all["dtm"]
+dt.wf.raw_job_type.hat.all <- list.raw_job_type.hat.all[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.raw_job_type.hat.all, freq > 100), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+## save
+save(list.title.hat.all, list.abstract.hat.all, list.raw_job_type.hat.all, file = "../../../data/RData/dt_keywords_hat_all.RData")
+
+#######################################################################################
+## hat == -1 ##########################################################################
+#######################################################################################
+# dt.hat.test <- dt.jobs[hat == -1]
+title.hat.test <- tolower(dt.jobs[hat == -1]$title)
+abstract.hat.test <- tolower(dt.jobs[hat == -1]$abstract)
+raw_job_type.hat.test <- tolower(dt.jobs[hat == -1]$raw_job_type)
+
+###########
+## title ##
+###########
+list.title.hat.test <- txtPreprocess(title.hat.test, rm_sparse = .9999)
+dt.title.hat.test <- list.title.hat.test[["dt"]]
+dtm.title.hat.test <- list.title.hat.test["dtm"]
+dt.wf.title.hat.test <- list.title.hat.test[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.title.hat.test, freq > 2000), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+##############
+## abstract ##
+##############
+list.abstract.hat.test <- txtPreprocess(abstract.hat.test, rm_sparse = .9999)
+dt.abstract.hat.test <- list.abstract.hat.test[["dt"]]
+dtm.abstract.hat.test <- list.abstract.hat.test["dtm"]
+dt.wf.abstract.hat.test <- list.abstract.hat.test[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.abstract.hat.test, freq > 2000), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+##################
+## raw_job_type ##
+##################
+list.raw_job_type.hat.test <- txtPreprocess(raw_job_type.hat.test, rm_sparse = .9999)
+dt.raw_job_type.hat.test <- list.raw_job_type.hat.test[["dt"]]
+dtm.raw_job_type.hat.test <- list.raw_job_type.hat.test["dtm"]
+dt.wf.raw_job_type.hat.test <- list.raw_job_type.hat.test[["dt.wf"]]
+
+# plot
+p <- ggplot(subset(dt.wf.raw_job_type.hat.test, freq > 100), aes(word, freq))
+p <- p + geom_bar(stat = "identity")   
+p <- p + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+p
+
+## save
+save(list.title.hat.test, list.abstract.hat.test, list.raw_job_type.hat.test, file = "../../../data/RData/dt_keywords_hat_test.RData")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ kaggle/noah is a soy sause/script/utility.R
@@ -0,0 +1,98 @@
+#######################################################################################
+## txtPreprocess ######################################################################
+#######################################################################################
+txtPreprocess <- function(x, rm_sparse = 1){
+    require(slam)
+    cat("stemming ...\n")
+    x <- wordStem(x)
+    cat("removing punctuations and digits ...\n")
+    x <- gsub("[[:punct:][:digit:]]", " ", x)
+    cat("removing more than 2 space ...\n")
+    x <- gsub("\\s+", " ", x)
+    cat("removing non alpha ...\n")
+    x <- gsub("[^a-zA-Z]", " ", x)
+    cat("removing more than 2 space and strim spaces ...\n")
+    x <- str_trim(gsub("\\s+", " ", x))
+    cat("removing stop words ...\n")
+    x <- removeWords(x, stopwords(kind = "en"))
+    # corp.x <- tm_map(Corpus(VectorSource(x)), contentremoveWords, stopwords("english")) 
+    cat("stemming ...\n")
+    # x <- stemDocument(x)
+    corp.x <- tm_map(Corpus(VectorSource(x)), content_transformer(stemDocument), lazy = T)
+    
+    cat("Creating data-term-matrix (dtm) ...\n")
+    corp.x <- tm_map(corp.x, #Corpus(VectorSource(x)),
+                     content_transformer(function(x) iconv(x, to = 'UTF-8', sub = 'byte')),
+                     mc.cores = 1)
+    dtm.x <- DocumentTermMatrix(corp.x, control = list(minWordLength = 2, minDocFreq = 3))
+    
+    if(rm_sparse != 1){
+        cat("removing sparse ...\n")
+        dtm.x <- removeSparseTerms(dtm.x, rm_sparse)
+    }
+    
+    cat("Creating data table (dt) ...\n")
+    dt.x <- data.table(text = unlist(sapply(corp.x, `[`, "content")))
+    
+    ## freq
+    freq.x <- col_sums(as.matrix(dtm.x), na.rm = T)
+    length(freq.x); length(x)
+    
+    # order by frequency
+    ord.x <- order(-freq.x)
+    cat("head word frequency:]n")
+    print(head(freq.x[ord.x]))
+    
+    cat("Creating word-frequency data table (dt.wf) ...\n")
+    dt.wf.x <- data.table(word = names(freq.x), freq = freq.x)[order(-freq.x)]
+    dt.wf.x
+    
+    return(list(dt = dt.x, dtm = dtm.x, dt.wf = dt.wf.x))
+}
+
+
+
+# require(slam)
+# cat("stemming ...\n")
+# x <- wordStem(raw_job_type.hat.all)
+# cat("removing punctuations and digits ...\n")
+# x <- gsub("[[:punct:][:digit:]]", " ", x)
+# cat("removing more than 2 space ...\n")
+# x <- gsub("\\s+", " ", x)
+# cat("removing non alpha ...\n")
+# x <- gsub("[^a-zA-Z]", " ", x)
+# cat("removing more than 2 space and strim spaces ...\n")
+# x <- str_trim(gsub("\\s+", " ", x))
+# cat("removing stop words ...\n")
+# x <- removeWords(x, stopwords(kind = "en"))
+# # corp.x <- tm_map(Corpus(VectorSource(x)), contentremoveWords, stopwords("english")) 
+# cat("stemming ...\n")
+# # x <- stemDocument(x)
+# corp.x <- tm_map(Corpus(VectorSource(x)), content_transformer(stemDocument), lazy = T)
+# 
+# cat("Creating data-term-matrix (dtm) ...\n")
+# corp.x <- tm_map(corp.x, #Corpus(VectorSource(x)),
+#                  content_transformer(function(x) iconv(x, to = 'UTF-8', sub = 'byte')),
+#                  mc.cores = 1)
+# dtm.x <- DocumentTermMatrix(corp.x, control = list(minWordLength = 2, minDocFreq = 3))
+# 
+# dt.x <- as.data.table(data.frame(text = unlist(sapply(corp.x, `[`, "content")), stringsAsFactors = F))
+# 
+# dtm.x.x <- removeSparseTerms(dtm.x, .999)
+# cat("Creating data table (dt) ...\n")
+# dt.x <- data.table(text = unlist(sapply(corp.x, `[`, "content")))
+# 
+# ## freq
+# freq.x <- col_sums(as.matrix(dtm.x.x), na.rm = T)
+# length(freq.x); length(x)
+# 
+# # order by frequency
+# ord.x <- order(-freq.x)
+# cat("head word frequency:]n")
+# print(head(freq.x[ord.x]))
+# 
+# cat("Creating word-frequency data table (dt.wf) ...\n")
+# dt.wf.x <- data.table(word = names(freq.x), freq = freq.x)[order(-freq.x)]
+# dt.wf.x
+# 
+# list.raw_job_tyype.hat.all <- list(dt = dt.x, dtm = dtm.x, dt.wf = dt.wf.x)
