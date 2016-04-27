ce3fe2ddf73e6bd813be6f26fd94085dc10afdf7
Noah Xiao <noah.xm.shaw@gmail.com>
2016-04-27 16:04:09 +1000

    Update 4_getLocation.R

--- kaggle/noah is a soy sause/script/4_getLocation.R
+++ kaggle/noah is a soy sause/script/4_getLocation.R
@@ -1,28 +1,35 @@
-# setwd("/Volumes/Data Science/Google Drive/data_science_competition/melbourne_datathon_2nd/Melbourne_Datathon_2016/kaggle/noah is a soy sause/")
-rm(list = ls()); gc();
-require(data.table)
 require(XML)
-load("dt_jobs.RData")
-#######################################################################################
-## clean raw location #################################################################
-#######################################################################################
-## location with  g_id
-loc.id.proper <- dt.jobs$location_id[grepl("g_id:[[:digit:]]", dt.jobs$location_id)]
-loc.id.proper.substr <- unique(substr(loc.id.proper, 6, nchar(loc.id.proper)))
-ls.xml <- list()
-for(loc in loc.id.proper.substr){
-    path <- paste0("http://api.geonames.org/get?geonameId=", loc, "$maxRows=1&username=noahhhhhh")
-    xml <- xmlParse(path)
-    ls.xml[[loc]] <- xmlToList(xml)
-}
+setwd("../Desktop")
+load("dt_g_id.RData")
+load("dt_zip.RData")
+load("dt_location_id.RData")
+require(data.table)
+dt.location_id
+dt.g_id
+dt.zip
 
-## location with g_zip
-loc.zip.proper <- dt.jobs$location_id[grepl("g_zip:", dt.jobs$location_id)]
-loc.zip.proper.substr <- unique(as.numeric(gsub("[^\\d]+", "", loc.zip.proper, perl = T)))
 
+## refine the dt.zip
+dt.zip
 ls.xml.zip <- list()
-for(loc in loc.zip.proper.substr){
-    path.zip <- paste0("http://api.geonames.org/postalCodeSearch?postalcode=", loc, "&maxRows=1&username=noahhhhhh")
-    xml.zip <- xmlParse(path.zip)
-    ls.xml.zip[[loc]] <- xmlToList(xml.zip)
+z <- 1
+for(loc in dt.zip$loc_name){
+  path.zip <- paste0("http://api.geonames.org/search?name=", loc, "&country=AU&maxRows=1&username=noahhhhhh")
+  xml.zip <- xmlParse(path.zip)
+  ls.xml.zip[[loc]] <- xmlToList(xml.zip)
+  print(paste(z, "in", nrow(dt.zip)))
 }
+
+
+## refine the dt.g_id
+dt.g_id
+ls.xml.g_id <- list()
+g <- 1
+for(loc in substr(dt.g_id$location_id, 6, nchar(dt.g_id$location_id))){
+  path.g_id <- paste0("http://api.geonames.org/get?geonameId=", loc, "&username=noahhhhhh")
+  xml.g_id<- xmlParse(path.g_id)
+  ls.xml.g_id[[loc]] <- xmlToList(xml.g_id)
+  print(paste(g, "in", nrow(dt.g_id)))
+  g <- g + 1
+}
+
